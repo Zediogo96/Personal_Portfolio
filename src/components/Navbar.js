b4ffe770bf20605476dff1e4562a48715f639023
Zediogo96 <zediogox@hotmail.com>
2023-01-04 14:56:32 +0000

    Navbar fully complete, including responsiveness

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import "./Navbar.css"
+
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+    return (
+        <div className="header">
+            <Link to={"/"}>
+                <h1> Portfolio </h1>
+            </Link>
+
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+
+                <li>
+                    <Link to={"/"}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to={"/About"}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to={"/Contact"}>
+                        Contact
+                    </Link>
+                </li>
+                <li>
+                    <Link to={"/Projects"}>
+                        Projects
+                    </Link>
+                </li>
+            </ul>
+
+            <div className="hamburger-menu" onClick={handleClick}>
+                {click ? <FaTimes size={20} style={{ color: "#fff" }} /> : <FaBars size={20} style={{ color: "#fff" }} />}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
